356dfccec0fc33f411895bd5ea6646149e8affd5
Nihit Agarwal <nihitagarwal0@gmail.com>
2026-05-12 16:23:43 +0530

    Record page added

---
+++ src/app/record/record.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-record',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './record.html',
+  styleUrl: './record.css',
+})
+export class Record {
+
+}
